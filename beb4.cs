f98055b69005ca434f368f5c8055e3928444dec2
Noremorce <65261136+Noremorce@users.noreply.github.com>
2020-05-29 21:45:21 +0500

    Add files via upload

---
+++ beb4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+namespace zadanie4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList obj = new ArrayList() { 16, 22.8, 1, "a", 6.6, "b", "C", "D",666, 2, "f", "G", 66, 15.5, 0 };
+            ArrayList objUpper = new ArrayList(100);
+
+            for (int i = 0; i < obj.Count; i++)
+            {
+                Console.Write(obj[i] + " | ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < obj.Count; i++)
+            {
+                if (obj[i] is string)
+                {
+                    obj.RemoveAt(i);
+                    if (obj[i] is string) i--;
+                }
+            }
+
+            for (int i = 0; i < obj.Count; i++)
+            {
+                Console.Write(obj[i] + " | ");
+            }
+        }
+    }
+}
